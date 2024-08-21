3fe34031bbd6994408679f979a5041e05db791ce
Celio Grand <celiogrand@outlook.com>
2024-08-22 06:08:27 +0800

    arc mutex stream

--- src/amnis.rs
+++ src/amnis.rs
@@ -5,10 +5,10 @@ use crate::gas::GasPlan;
 use crate::io::OutputChunk;
 
 #[async_trait::async_trait]
-pub trait Amnis<'a> {
-    fn new(gas_plan: GasPlan, input: BoxStream<'a, Bytes>) -> Self
+pub trait Amnis {
+    fn new(gas_plan: GasPlan, input: BoxStream<'static, Bytes>) -> Self
     where
         Self: Sized;
 
-    async fn handle(&mut self) -> BoxStream<OutputChunk>;
+    async fn handle(&self) -> BoxStream<OutputChunk>;
 }
--- src/core.rs
+++ src/core.rs
@@ -10,39 +10,45 @@ use crate::{
     Amnis, Variable,
 };
 
-pub struct AmnisState<'a> {
+pub struct AmnisState {
     gas_plan: GasPlan,
     gas_used: Gas,
     variables: HashMap<String, Variable>,
     channels: HashMap<i32, Channel>,
-    input: BoxStream<'a, Bytes>,
 }
 
-pub struct AmnisCore<'a> {
-    state: Arc<Mutex<AmnisState<'a>>>,
+pub struct AmnisCore {
+    state: Arc<Mutex<AmnisState>>,
+    input: Arc<Mutex<BoxStream<'static, Bytes>>>,
 }
 
 #[async_trait::async_trait]
-impl<'a> Amnis<'a> for AmnisCore<'a> {
-    fn new(gas_plan: GasPlan, input: BoxStream<'a, Bytes>) -> Self {
+impl Amnis for AmnisCore {
+    fn new(gas_plan: GasPlan, input: BoxStream<'static, Bytes>) -> Self {
         AmnisCore {
             state: Arc::new(Mutex::new(AmnisState {
                 gas_plan,
                 gas_used: Gas::zero(),
                 variables: HashMap::new(),
-                channels: HashMap::from([(0, Channel::new())]),
-                input,
+                channels: HashMap::from([(1, Channel::new())]),
             })),
+            input: Arc::new(Mutex::new(input)),
         }
     }
 
     /// Handle a bytes stream.
-    async fn handle(&mut self) -> BoxStream<OutputChunk> {
+    async fn handle(&self) -> BoxStream<OutputChunk> {
         // Channel for muxing outputs
         // TODO config channel size somewhere, some compile flag I guess
-        let (tx, mut rx) = mpsc::channel::<OutputChunk>(128);
-        // Fire channels and give them tx.clone()
-        tokio::spawn(AmnisCore::run(tx));
+        let (tx, mut rx) = mpsc::channel::<OutputChunk>(32);
+        // Feed inputs to channels
+        let state = self.state.clone();
+        let input = self.input.clone();
+        tokio::spawn((|| async move {
+            drop(tx);
+            drop(state);
+            drop(input);
+        })());
         // Early return the output stream
         Box::pin(async_stream::stream! {
             while let Some(item) = rx.recv().await {
@@ -50,8 +56,4 @@ impl<'a> Amnis<'a> for AmnisCore<'a> {
             }
         })
     }
-}
-
-impl AmnisCore<'_> {
-    async fn run(tx: Sender<OutputChunk>) {}
 }
